bb3bf72f5da30006b61cda52bc007383ef29dd41
QuangVMSE192755 <vminhquang05@gmail.com>
2025-09-30 14:10:15 +0700

    Thêm hàm logout

---
+++ Backend/src/main/java/com/swp391/gr3/ev_management/DTO/response/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.swp391.gr3.ev_management.DTO.response;
+
+import com.swp391.gr3.ev_management.entity.Users;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+    private Users user;
+}
--- Backend/src/main/java/com/swp391/gr3/ev_management/service/UserService.java
+++ Backend/src/main/java/com/swp391/gr3/ev_management/service/UserService.java
@@ -3,6 +3,7 @@ package com.swp391.gr3.ev_management.service;
 import com.swp391.gr3.ev_management.DTO.request.LoginRequest;
 import com.swp391.gr3.ev_management.DTO.request.RegisterRequest;
 import com.swp391.gr3.ev_management.entity.Users;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
@@ -13,4 +14,7 @@ public interface UserService {
     public Users login(LoginRequest loginRequest);
     public boolean existsByPhoneNumber(String phoneNumber);
     public boolean existsByEmail(String email);
+    ResponseEntity<?> logout(HttpServletRequest request);
+    Users authenticate(String phoneNumber, String password);
+
 }
