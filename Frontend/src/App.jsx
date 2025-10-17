e8cd334bc92d38d133b6ed365a90e8e237cb7494
hoaibao1102 <112604235+hoaibao1102@users.noreply.github.com>
2025-10-17 15:07:09 +0700

    regis and login form

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,12 +4,11 @@ import Register from "./pages/auth/Register.jsx";
 import Home from "./pages/home/Home.jsx";
 import Header from "./pages/header/Header.jsx";
 import Footer from "./pages/footer/Footer.jsx";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <Header />
       {/* Routes */}
       <div className="content">
         <Routes>
@@ -18,7 +17,6 @@ function App() {
           <Route path="/register" element={<Register />} />
         </Routes>
       </div>
-      <Footer />
     </div>
   );
 }
