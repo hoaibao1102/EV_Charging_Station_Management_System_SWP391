d0fbbcb27af852698a2ea012f132e2250d50703f
PhamAnh <thianhpham130204@gmail.com>
2025-10-23 21:17:29 +0700

    phân layout , router

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,15 +1,17 @@
-
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import router from "./path/router.jsx";
+import { createRoot } from 'react-dom/client';
+import AppRouter from './path/AppRouter.jsx'
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
         <>
-                <RouterProvider router={router} /> 
+        <Provider store={store}>
+                <AppRouter />
                 <ToastContainer position="top-center" autoClose={2000} />
+        </Provider>
         </>
 );
---
+++ Frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getAuthData, isAuthenticated } from '../../utils/authUtils.js'; 
+
+// Lấy dữ liệu bền vững từ Local Storage để khôi phục trạng thái
+const initialData = getAuthData();
+
+const initialState = {
+  isLoggedIn: isAuthenticated(), // Kiểm tra trạng thái đăng nhập
+  user: null, // Thông tin chi tiết của người dùng (ID, username, email, phone...)
+  role: initialData.role || null, // Vai trò (ADMIN, STAFF, DRIVER)
+  accessToken: initialData.accessToken || null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.isLoggedIn = true;
+      state.user = action.payload.userDetails;
+      state.role = action.payload.role; 
+      state.accessToken = action.payload.accessToken;
+    },
+    logout: (state) => {
+      state.isLoggedIn = false;
+      state.user = null;
+      state.role = null;
+      state.accessToken = null;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+
+export const selectRole = (state) => state.auth.role;
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+
+export default authSlice.reducer;
