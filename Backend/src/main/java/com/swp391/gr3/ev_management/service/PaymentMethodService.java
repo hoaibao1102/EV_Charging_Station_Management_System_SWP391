d5722935bb5a53db64cec8751257cf825b9a6620
QuangVMSE192755 <vminhquang05@gmail.com>
2025-11-05 10:01:02 +0700

    thêm paymentmethods

--- Backend/src/main/java/com/swp391/gr3/ev_management/service/PaymentMethodService.java
+++ Backend/src/main/java/com/swp391/gr3/ev_management/service/PaymentMethodService.java
@@ -1,11 +1,13 @@
 package com.swp391.gr3.ev_management.service;
 
+import com.swp391.gr3.ev_management.DTO.response.PaymentMethodResponse;
 import com.swp391.gr3.ev_management.entity.PaymentMethod;
 import com.swp391.gr3.ev_management.enums.PaymentProvider;
 import com.swp391.gr3.ev_management.enums.PaymentType;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public interface PaymentMethodService {
@@ -20,4 +22,6 @@ public interface PaymentMethodService {
                                              PaymentProvider provider,
                                              String accountNo,
                                              LocalDate expiryDate);
+
+    List<PaymentMethodResponse> getAllPaymentMethods();
 }
