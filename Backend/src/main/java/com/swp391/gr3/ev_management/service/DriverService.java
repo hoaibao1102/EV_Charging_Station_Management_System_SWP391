6a1c9559b83c533953a73030d4a7ec984b35403d
QuangVMSE192755 <vminhquang05@gmail.com>
2025-11-15 15:56:29 +0700

    fix password

--- Backend/src/main/java/com/swp391/gr3/ev_management/service/DriverService.java
+++ Backend/src/main/java/com/swp391/gr3/ev_management/service/DriverService.java
@@ -1,9 +1,6 @@
 package com.swp391.gr3.ev_management.service;
 
-import com.swp391.gr3.ev_management.dto.request.AddVehicleRequest;
-import com.swp391.gr3.ev_management.dto.request.DriverRequest;
-import com.swp391.gr3.ev_management.dto.request.DriverUpdateRequest;
-import com.swp391.gr3.ev_management.dto.request.UpdateVehicleRequest;
+import com.swp391.gr3.ev_management.dto.request.*;
 import com.swp391.gr3.ev_management.dto.response.ChargingSessionBriefResponse;
 import com.swp391.gr3.ev_management.dto.response.DriverResponse;
 import com.swp391.gr3.ev_management.dto.response.TransactionBriefResponse;
@@ -33,7 +30,7 @@ public interface DriverService {
     VehicleResponse updateVehicle(Long userId, Long vehicleId, UpdateVehicleRequest request);
     VehicleResponse updateVehicleStatus(Long userId, Long vehicleId, UserVehicleStatus status);
 
-    DriverResponse updateDriverPassword(Long userId, String oldPassword, String newPassword, String confirmNewPassword);
+    DriverResponse updateDriverPassword(Long userId, UpdatePasswordRequest request);
 
     List<TransactionBriefResponse> getMyTransactions(Long userId);
     List<ChargingSessionBriefResponse> getMyChargingSessions(Long userId);
